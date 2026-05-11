a34821191b1ab50f444d9c411b3934d373001edc
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2026-05-11 17:51:05 -0300

    feat: implement room persistence layer

---
+++ src/main/java/com/coworking/coworkingreservation/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.coworking.coworkingreservation.repository;
+
+import com.coworking.coworkingreservation.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
