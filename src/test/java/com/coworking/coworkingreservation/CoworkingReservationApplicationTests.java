525ada85b57fcf717a24fd8a6cd63a3539a6b802
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2026-05-11 15:25:48 -0300

    chore: initialize Spring Boot project

---
+++ src/test/java/com/coworking/coworkingreservation/CoworkingReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coworking.coworkingreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
