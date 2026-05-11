93f0ad124467cd6ad5fdb9b02fddbef31c620c2d
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2026-05-11 15:43:17 -0300

    feat: configure H2 database and JPA settings

--- src/main/java/com/coworking/coworkingreservation/CoworkingReservationApplication.java
+++ src/main/java/com/coworking/coworkingreservation/CoworkingReservationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoworkingReservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoworkingReservationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoworkingReservationApplication.class, args);
+    }
 
 }
